6edb3a2f7b2333276c106d41c4fe1e513769eff1
Zuko <Zuko@Thao-AMDr5>
2025-07-18 22:44:41 +0700

    PS-2024.1.2 <Zuko@Thao-AMDr5 Update PHP File Header.php, rainbow_brackets.xml, githubActionCache.xml

--- fileTemplates/includes/PHP File Header.php
+++ fileTemplates/includes/PHP File Header.php
@@ -2,13 +2,13 @@
  * --------------------------------------------------------------------------
  *
  * --------------------------------------------------------------------------
- * @PROJECT    : ${PROJECT_NAME}
- * @AUTHOR     : Zuko <https://github.com/tansautn>
- * @LINK       : https://www.zuko.pro/
- * @FILE       :#if (${FILE_NAME}) ${FILE_NAME}
+ * ~PROJECT    : ${PROJECT_NAME}
+ * ~AUTHOR     : Zuko <https://github.com/tansautn>
+ * ~LINK       : https://www.zuko.pro/
+ * ~FILE       :#if (${FILE_NAME}) ${FILE_NAME}
 #else ${NAME}.${EXT}
 #end
- * @CREATED    : ${TIME} , ${DAY}/${MONTH_NAME_SHORT}/${YEAR}
-#if($VERSION) * @VERSION    : ${VERSION}
+ * ~CREATED    : ${TIME} , ${DAY}/${MONTH_NAME_SHORT}/${YEAR}
+#if($VERSION) * ~VERSION    : ${VERSION}
 #end
  */
